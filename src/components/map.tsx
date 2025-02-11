51fe55905a63c70d8f90c5d54c746c0f2aee79d7
Thibaut <thibautstachnick@gmail.com>
2025-02-11 16:28:16 +0100

    lint files

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,262 +1,319 @@
 "use client";
 import React, { useEffect, useState, useContext } from "react";
 import { WebSocketContext } from "@/providers/websocket-provider";
-import { GoogleMap, Marker, DirectionsRenderer, Autocomplete } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  Marker,
+  DirectionsRenderer,
+  Autocomplete,
+} from "@react-google-maps/api";
+import axios from "axios";
 
 export const defaultMapContainerStyle = {
-    width: "100%",
-    height: "80vh",
-    borderRadius: "15px 0px 0px 15px",
+  width: "100%",
+  height: "60vh",
+  borderRadius: "15px",
 };
 
 const defaultMapZoom = 15;
 const defaultMapOptions = {
-    zoomControl: true,
-    tilt: 0,
-    gestureHandling: "auto",
-    mapTypeId: "roadmap",
-    mapId: "55de5befcc72c4e9",
+  zoomControl: true,
+  tilt: 0,
+  gestureHandling: "auto",
+  mapTypeId: "roadmap",
+  mapId: "55de5befcc72c4e9",
 };
 
 interface PlaceData {
-    autocomplete?: google.maps.places.Autocomplete | null;
-    value: string | undefined;
-    location?: google.maps.LatLngLiteral;
+  autocomplete?: google.maps.places.Autocomplete | null;
+  value: string | undefined;
+  location?: google.maps.LatLngLiteral;
 }
 
+const getDataFromIp = async () => {
+  const response = await axios.get("https://us1.api-bdc.net/data/client-ip");  
+  const data = await response.data;
+  const ip = data.ipString;
+  const coords = await axios(`https://ipapi.co/${ip}/json/`);
+  const coordsData = await coords.data;
+  return coordsData;
+};
+
 const MapComponent = () => {
-    const { userCount, positions } = useContext(WebSocketContext);
-    const [newPositions, setNewPositions] = useState<Array<{ lat: number; lng: number }>>([]);
-    const [mapCenter, setMapCenter] = useState<google.maps.LatLngLiteral>({
-        lat: 35.8799866,
-        lng: 76.5048004,
-    });
+  const { userCount, positions } = useContext(WebSocketContext);
+  const [newPositions, setNewPositions] = useState<
+    Array<{ lat: number; lng: number }>
+  >([]);
+
+  
+  const [coords, setCoords] = useState<{lat: number, lng: number}>({lat: 0, lng: 0});
+  const [mapCenter, setMapCenter] = useState<google.maps.LatLngLiteral>(coords);
 
     useEffect(() => {
-        if (positions && mapCenter) {
-            const filteredPositions = positions.filter(
-                (pos) => pos.lat !== mapCenter.lat || pos.lng !== mapCenter.lng
-            );
-            setNewPositions(filteredPositions);
-        }
-    }, [positions, mapCenter]);
-
-    const [directionsResponse, setDirectionsResponse] = useState<google.maps.DirectionsResult | null>(null);
-    const [destination, setDestination] = useState<PlaceData>({
-        value: "",
-        autocomplete: null,
-    });
-    const [waypoints, setWaypoints] = useState<PlaceData[]>([]);
-
-    const fetchRoute = (
-        origin: google.maps.LatLngLiteral,
-        destinationLoc: google.maps.LatLngLiteral,
-        waypointLocations: google.maps.LatLngLiteral[]
-    ) => {
-        const directionsService = new google.maps.DirectionsService();
-        directionsService.route(
-            {
-                origin,
-                destination: destinationLoc,
-                travelMode: google.maps.TravelMode.WALKING,
-                waypoints: waypointLocations.map((p) => ({
-                    location: p,
-                    stopover: true,
-                })),
-            },
-            (result, status) => {
-                if (status === google.maps.DirectionsStatus.OK && result) {
-                    setDirectionsResponse(result);
-                } else {
-                    alert(`Échec de la requête d'itinéraire : ${status}`);
-                }
-            }
-        ).catch()
-    };
-
-    useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                const currentPosition = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                };
-                setMapCenter(currentPosition);
-            });
-        }
+      if (!navigator.geolocation){
+        const fetchCoords = async () => {
+          const coordsData = await getDataFromIp();
+          setCoords({
+            lat: coordsData.latitude,
+            lng: coordsData.longitude
+          });
+          setMapCenter({
+            lat: coordsData.latitude,
+            lng: coordsData.longitude
+          });
+        };
+  
+        fetchCoords();
+      }
     }, []);
+  
 
-    const handleCreateRoute = () => {
-        if (!destination.location) {
-            return alert("Veuillez choisir une destination via l'autocomplete.");
-        }
-        const validWaypoints: google.maps.LatLngLiteral[] = [];
-        for (const wp of waypoints) {
-            if (wp.location) {
-                validWaypoints.push(wp.location);
-            }
-        }
-        fetchRoute(mapCenter, destination.location, validWaypoints);
-    };
+  useEffect(() => {
+    if (positions && mapCenter) {
+      const filteredPositions = positions.filter(
+        (pos) => pos.lat !== mapCenter.lat || pos.lng !== mapCenter.lng,
+      );
+      setNewPositions(filteredPositions);
+    }
+  }, [positions, mapCenter]);
 
-    const onLoadDestinationAutocomplete = (autocomplete: google.maps.places.Autocomplete) => {
-        setDestination((prev) => ({ ...prev, autocomplete }));
-    };
+  const [directionsResponse, setDirectionsResponse] =
+    useState<google.maps.DirectionsResult | null>(null);
+  const [destination, setDestination] = useState<PlaceData>({
+    value: "",
+    autocomplete: null,
+  });
+  const [waypoints, setWaypoints] = useState<PlaceData[]>([]);
 
-    const onPlaceChangedDestination = () => {
-        if (destination.autocomplete) {
-            const place = destination.autocomplete.getPlace();
-            if (place?.geometry?.location) {
-                const location = place.geometry.location;
-                setDestination((prev) => ({
-                    ...prev,
-                    value: place.formatted_address || place.name || "",
-                    location: {
-                        lat: location.lat(),
-                        lng: location.lng(),
-                    },
-                }));
-            } else {
-                alert("Veuillez sélectionner une destination valide.");
-            }
-        }
-    };
+  const fetchRoute = (
+    origin: google.maps.LatLngLiteral,
+    destinationLoc: google.maps.LatLngLiteral,
+    waypointLocations: google.maps.LatLngLiteral[],
+  ) => {
+    const directionsService = new google.maps.DirectionsService();
+    directionsService
+      .route(
+        {
+          origin,
+          destination: destinationLoc,
+          travelMode: google.maps.TravelMode.WALKING,
+          waypoints: waypointLocations.map((p) => ({
+            location: p,
+            stopover: true,
+          })),
+        },
+        (result, status) => {
+          if (status === google.maps.DirectionsStatus.OK && result) {
+            setDirectionsResponse(result);
+          } else {
+            alert(`Échec de la requête d'itinéraire : ${status}`);
+          }
+        },
+      )
+      .catch();
+  };
 
-    const handleAddWaypoint = () => {
-        setWaypoints((prev) => [
-            ...prev,
-            { value: "", autocomplete: null, location: undefined },
-        ]);
-    };
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const currentPosition = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        setMapCenter(currentPosition);
+      });
+    }
+  }, []);
 
-    const onLoadWaypointAutocomplete = (autocomplete: google.maps.places.Autocomplete, index: number) => {
+  const handleCreateRoute = () => {
+    if (!destination.location) {
+      return alert("Veuillez choisir une destination via l'autocomplete.");
+    }
+    const validWaypoints: google.maps.LatLngLiteral[] = [];
+    for (const wp of waypoints) {
+      if (wp.location) {
+        validWaypoints.push(wp.location);
+      }
+    }
+    fetchRoute(mapCenter, destination.location, validWaypoints);
+  };
+
+  const onLoadDestinationAutocomplete = (
+    autocomplete: google.maps.places.Autocomplete,
+  ) => {
+    setDestination((prev) => ({ ...prev, autocomplete }));
+  };
+
+  const onPlaceChangedDestination = () => {
+    if (destination.autocomplete) {
+      const place = destination.autocomplete.getPlace();
+      if (place?.geometry?.location) {
+        const location = place.geometry.location;
+        setDestination((prev) => ({
+          ...prev,
+          value: place.formatted_address || place.name || "",
+          location: {
+            lat: location.lat(),
+            lng: location.lng(),
+          },
+        }));
+      } else {
+        alert("Veuillez sélectionner une destination valide.");
+      }
+    }
+  };
+
+  const handleAddWaypoint = () => {
+    setWaypoints((prev) => [
+      ...prev,
+      { value: "", autocomplete: null, location: undefined },
+    ]);
+  };
+
+  const onLoadWaypointAutocomplete = (
+    autocomplete: google.maps.places.Autocomplete,
+    index: number,
+  ) => {
+    setWaypoints((prev) => {
+      const newWaypoints = [...prev];
+      newWaypoints[index].autocomplete = autocomplete;
+      return newWaypoints;
+    });
+  };
+
+  const onPlaceChangedWaypoint = (index: number) => {
+    const wp = waypoints[index];
+    if (wp.autocomplete) {
+      const place = wp.autocomplete.getPlace();
+      if (place?.geometry?.location) {
+        const location = place.geometry.location;
         setWaypoints((prev) => {
-            const newWaypoints = [...prev];
-            newWaypoints[index].autocomplete = autocomplete;
-            return newWaypoints;
+          const newWaypoints = [...prev];
+          newWaypoints[index] = {
+            ...newWaypoints[index],
+            value: place.formatted_address || place.name || "",
+            location: {
+              lat: location.lat(),
+              lng: location.lng(),
+            },
+          };
+          return newWaypoints;
         });
-    };
+      } else {
+        alert("Veuillez sélectionner un point de passage valide.");
+      }
+    }
+  };
 
-    const onPlaceChangedWaypoint = (index: number) => {
-        const wp = waypoints[index];
-        if (wp.autocomplete) {
-            const place = wp.autocomplete.getPlace();
-            if (place?.geometry?.location) {
-                const location = place.geometry.location;
-                setWaypoints((prev) => {
-                    const newWaypoints = [...prev];
-                    newWaypoints[index] = {
-                        ...newWaypoints[index],
-                        value: place.formatted_address || place.name || "",
-                        location: {
-                            lat: location.lat(),
-                            lng: location.lng(),
-                        },
-                    };
-                    return newWaypoints;
-                });
-            } else {
-                alert("Veuillez sélectionner un point de passage valide.");
-            }
-        }
-    };
-
-    return (
-        <div className="w-full p-4">
-            <div className="mb-4 p-4 bg-gray-100 rounded">
-                <h2 className="text-xl text-black font-bold mb-2">Créer un itinéraire</h2>
-                <div className="mb-4">
-                    <label htmlFor="destination" className="block text-black font-medium mb-1">
-                        Destination :
-                    </label>
-                    <Autocomplete
-                        onLoad={onLoadDestinationAutocomplete}
-                        onPlaceChanged={onPlaceChangedDestination}
-                    >
-                        <input
-                            name="destination"
-                            type="text"
-                            placeholder="Cherchez votre destination"
-                            className="w-full p-2 border rounded text-black"
-                        />
-                    </Autocomplete>
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="point de passage" className="block text-black font-medium mb-1">
-                        Points de passage :
-                    </label>
-                    {waypoints.map((_, idx) => (
-                        <div key={'waypoint'} className="mb-2">
-                            <Autocomplete
-                                onLoad={(autocomplete) => onLoadWaypointAutocomplete(autocomplete, idx)}
-                                onPlaceChanged={() => onPlaceChangedWaypoint(idx)}
-                            >
-                                <input
-                                    name="point de passage"
-                                    type="text"
-                                    placeholder={`Point de passage ${idx + 1}`}
-                                    className="w-full p-2 border rounded text-black"
-                                />
-                            </Autocomplete>
-                        </div>
-                    ))}
-                    <button
-                        type="button"
-                        onClick={handleAddWaypoint}
-                        className="mt-2 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600"
-                    >
-                        Ajouter un point de passage
-                    </button>
-                </div>
-                <button
-                    type="button"
-                    onClick={handleCreateRoute}
-                    className="py-2 px-4 bg-green-500 text-white rounded hover:bg-green-600"
-                >
-                    Créer l&#39;itinéraire
-                </button>
-                <br />
-                <span className="text-black">
-                    Nombres utilisateurs actuellement connectés : {userCount}
-                </span>
-            </div>
-            <GoogleMap
-                mapContainerStyle={defaultMapContainerStyle}
-                center={mapCenter}
-                zoom={defaultMapZoom}
-                options={defaultMapOptions}
-            >
-                <Marker position={mapCenter} title="Votre position" />
-                {destination.location && (
-                    <Marker
-                        position={destination.location}
-                        title="Destination"
-                        icon="https://maps.google.com/mapfiles/ms/icons/green-dot.png"
-                    />
-                )}
-                {waypoints.map(
-                    (wp, idx) =>
-                        wp.location && wp.location !== mapCenter && (
-                            <Marker
-                                key={'marker'}
-                                position={wp.location}
-                                title={`Point de passage ${idx + 1}`}
-                                icon="https://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-                            />
-                        )
-                )}
-                {newPositions?.map((position, idx) => (
-                    <Marker
-                        key={`user-position-${position.lat}-${position.lng}`}
-                        position={position}
-                        title={`User ${idx + 1}`}
-                        icon="https://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
-                    />
-                ))}
-                {directionsResponse && <DirectionsRenderer directions={directionsResponse} />}
-            </GoogleMap>
+  return (
+    <div className="w-full h-full p-4">
+      <div className="mb-4 p-4 bg-gray-100 round15">
+        <h2 className="text-xl text-black font-bold mb-2">
+          Créer un itinéraire
+        </h2>
+        <div className="mb-4">
+          <label
+            htmlFor="destination"
+            className="block text-black font-medium mb-1"
+          >
+            Destination :
+          </label>
+          <Autocomplete
+            onLoad={onLoadDestinationAutocomplete}
+            onPlaceChanged={onPlaceChangedDestination}
+          >
+            <input
+              name="destination"
+              type="text"
+              placeholder="Cherchez votre destination"
+              className="w-full p-2 border rounded text-black"
+            />
+          </Autocomplete>
         </div>
-    );
+        <div className="mb-4">
+          <label
+            htmlFor="point de passage"
+            className="block text-black font-medium mb-1"
+          >
+            Points de passage :
+          </label>
+          {waypoints.map((_, idx) => (
+            <div key={"waypoint"} className="mb-2">
+              <Autocomplete
+                onLoad={(autocomplete) =>
+                  onLoadWaypointAutocomplete(autocomplete, idx)
+                }
+                onPlaceChanged={() => onPlaceChangedWaypoint(idx)}
+              >
+                <input
+                  name="point de passage"
+                  type="text"
+                  placeholder={`Point de passage ${idx + 1}`}
+                  className="w-full p-2 border rounded text-black"
+                />
+              </Autocomplete>
+            </div>
+          ))}
+          <button
+            type="button"
+            onClick={handleAddWaypoint}
+            className="mt-2 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Ajouter un point de passage
+          </button>
+        </div>
+        <button
+          type="button"
+          onClick={handleCreateRoute}
+          className="py-2 px-4 bg-green-500 text-white rounded hover:bg-green-600"
+        >
+          Créer l&#39;itinéraire
+        </button>
+        <br />
+        <span className="text-black">
+          Nombres utilisateurs actuellement connectés : {userCount}
+        </span>
+      </div>
+      <GoogleMap
+        mapContainerStyle={defaultMapContainerStyle}
+        center={mapCenter}
+        zoom={defaultMapZoom}
+        options={defaultMapOptions}
+      >
+        <Marker position={mapCenter} title="Votre position" />
+        {destination.location && (
+          <Marker
+            position={destination.location}
+            title="Destination"
+            icon="https://maps.google.com/mapfiles/ms/icons/green-dot.png"
+          />
+        )}
+        {waypoints.map(
+          (wp, idx) =>
+            wp.location &&
+            wp.location !== mapCenter && (
+              <Marker
+                key={"marker"}
+                position={wp.location}
+                title={`Point de passage ${idx + 1}`}
+                icon="https://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+              />
+            ),
+        )}
+        {newPositions?.map((position, idx) => (
+          <Marker
+            key={`user-position-${position.lat}-${position.lng}`}
+            position={position}
+            title={`User ${idx + 1}`}
+            icon="https://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+          />
+        ))}
+        {directionsResponse && (
+          <DirectionsRenderer directions={directionsResponse} />
+        )}
+      </GoogleMap>
+    </div>
+  );
 };
 
 export { MapComponent };
--- src/providers/websocket-provider.tsx
+++ src/providers/websocket-provider.tsx
@@ -1,5 +1,5 @@
-"use client"
-import React, { createContext, useState, useEffect } from "react";
+"use client";
+import { createContext, useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import type { Socket } from "socket.io-client";
 
@@ -13,17 +13,17 @@ export const WebSocketContext = createContext<WebSocketContextType>({
   positions: [],
 });
 
-export const WebSocketProvider = ({ 
-  children 
-}: { 
-  children: React.ReactNode 
+export const WebSocketProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
 }) => {
   const [socket, setSocket] = useState<Socket | null>(null);
   const [userCount, setUserCount] = useState(0);
-  const [positions, setPositions] = useState<Array<{ lat: number; lng: number }>>(
-    []
-  );
-  console.log(socket)
+  const [positions, setPositions] = useState<
+    Array<{ lat: number; lng: number }>
+  >([]);
+  console.log(socket);
 
   useEffect(() => {
     const newSocket = io("https://nextmapws.thibautstachnick.com/");
@@ -37,9 +37,34 @@ export const WebSocketProvider = ({
     newSocket.on("updateData", (data) => {
       setUserCount((prev) => (prev !== data.userCount ? data.userCount : prev));
       setPositions((prev) =>
-        JSON.stringify(prev) !== JSON.stringify(data.positions) ? data.positions : prev
+        JSON.stringify(prev) !== JSON.stringify(data.positions)
+          ? data.positions
+          : prev,
       );
     });
+
+    if (!("geolocation" in navigator)) {
+      const fetchCoordsByIp = async () => {
+        try {
+          const response = await fetch("https://us1.api-bdc.net/data/client-ip");
+          const data = await response.json();
+          const ip = data.ipString;
+          const coords = await fetch(`https://ipapi.co/${ip}/json/`);
+          const coordsData = await coords.json();
+
+          const userPosition = {
+            lat: coordsData.latitude,
+            lng: coordsData.longitude,
+          };
+
+          newSocket.emit("updateLocation", userPosition);
+        } catch (error) {
+          console.error("Error getting location from IP:", error);
+        }
+      };
+
+      fetchCoordsByIp();
+    }
 
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
@@ -52,7 +77,7 @@ export const WebSocketProvider = ({
         },
         (error) => {
           console.error("Error getting location:", error);
-        }
+        },
       );
 
       const watchId = navigator.geolocation.watchPosition(
@@ -65,17 +90,23 @@ export const WebSocketProvider = ({
         },
         (error) => {
           console.error("Error watching location:", error);
-        }
+        },
       );
 
-      return () => navigator.geolocation.clearWatch(watchId);
+      return () => {
+        navigator.geolocation.clearWatch(watchId);
+        newSocket.off("initialData");
+        newSocket.off("updateData");
+        newSocket.disconnect();
+        console.log("terminated");
+      };
     }
 
     return () => {
       newSocket.off("initialData");
       newSocket.off("updateData");
       newSocket.disconnect();
-      console.log('terminated')
+      console.log("terminated");
     };
   }, []);
 
